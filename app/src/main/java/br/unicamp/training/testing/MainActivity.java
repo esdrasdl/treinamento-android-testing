1df691a2a2dc8f6e73a39c21dd718e8f11d36e21
Esdras Leite <esdras.leite@easytaxi.com.br>
2016-10-09 14:26:54 -0300

    Pre-Ex-2

--- app/src/main/java/br/unicamp/training/testing/MainActivity.java
+++ app/src/main/java/br/unicamp/training/testing/MainActivity.java
@@ -1,13 +1,41 @@
 package br.unicamp.training.testing;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextView;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.changeTextBt).setOnClickListener(this);
+        findViewById(R.id.activityChangeTextBtn).setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.textToBeChanged);
+        mEditText = (EditText) findViewById(R.id.editTextUserInput);
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Get the text from the EditText view.
+        final String text = mEditText.getText().toString();
+
+        switch (view.getId()) {
+            case R.id.changeTextBt:
+                mTextView.setText(text);
+                break;
+            case R.id.activityChangeTextBtn:
+                Intent intent = ShowTextActivity.newStartIntent(this, text);
+                startActivity(intent);
+                break;
+        }
     }
 }
